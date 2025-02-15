9861fd76afd7bd54f0caa814ba1589e637bdf4cc
RavikiranGoru <ravikirangoru@gmail.com>
2025-02-15 16:11:28 +0530

    Create nested components and arrow function

---
+++ react0_legacyway/src/example/Sample.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Mango = () => {
+
+    return (
+        <h2>Mangos are sweet</h2>
+    )
+}
+
+
+function Sample() {
+    return (
+        <div>
+            Sample
+            <Mango />
+        </div>
+    )
+}
+
+export default Sample
