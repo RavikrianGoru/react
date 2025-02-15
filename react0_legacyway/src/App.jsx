0bb2a4220468460eac5dac825c0089479ac1d03a
RavikiranGoru <ravikirangoru@gmail.com>
2025-02-15 17:02:38 +0530

    removed bioler plates files, JXS

---
+++ react0_legacyway/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './App.css'
+
+let a = "Apples"
+let b = 'Banana'
+
+function App() {
+  return (
+    <section>
+      <div>
+        <h1>JavaScript XML-1</h1>
+      </div>
+      <div>
+        <h1>JavaScript XML-2</h1>
+      </div>
+      <h1>I like {a} and {b}</h1>
+    </section>
+  )
+}
+
+export default App
