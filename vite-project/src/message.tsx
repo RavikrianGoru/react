63229fd0b0e4bd3a627477ccd24c70cc67f9e0c8
RavikiranGoru <ravikirangoru@gmail.com>
2025-03-09 13:31:46 +0530

    react with TypeScript project usinf vite

---
+++ vite-project/src/message.tsx
@@ -0,0 +1,14 @@
+
+//PascalCasing
+function Message() {
+    //jsx:JavaScript XML
+    const name = 'Ravi';
+    if (name) {
+        return <h1>Hellow {name} </h1>;
+    }
+    else {
+        return <h1>Hellow world </h1>;
+    }
+}
+
+export default Message;
